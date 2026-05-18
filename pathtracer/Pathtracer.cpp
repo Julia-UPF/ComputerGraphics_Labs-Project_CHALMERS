e68df6ff92da71a099e05c79305f84bc4942d89e
Julia-UPF <julia.casanovasi01@estudiant.upf.edu>
2026-05-18 19:23:13 +0200

    Finished pathtracer (with importance sampling) + Refractions

--- pathtracer/Pathtracer.cpp
+++ pathtracer/Pathtracer.cpp
@@ -69,33 +69,104 @@ vec3 Lenvironment(const vec3& wi)
 ///////////////////////////////////////////////////////////////////////////
 vec3 Li(Ray& primary_ray)
 {
+	bool inside = false;		//Added to keep track of whether the ray is inside a medium or not, for refraction
+	vec3 sigma_t = vec3(0.0f);	//Added to keep track of absorption in the medium for refraction
 	vec3 L = vec3(0.0f);
 	vec3 path_throughput = vec3(1.0);
 	Ray current_ray = primary_ray;
 
-	///////////////////////////////////////////////////////////////////
-	// Get the intersection information from the ray
-	///////////////////////////////////////////////////////////////////
-	Intersection hit = getIntersection(current_ray);
-	///////////////////////////////////////////////////////////////////
-	// Create a Material tree for evaluating brdfs and calculating
-	// sample directions.
-	///////////////////////////////////////////////////////////////////
+	for (int bounce = 0; bounce < settings.max_bounces; ++bounce) {		//ADDED for multiple bounces
+		///////////////////////////////////////////////////////////////////
+		// Get the intersection information from the ray
+		///////////////////////////////////////////////////////////////////
+		Intersection hit = getIntersection(current_ray);
+		if (inside) {
+			float distance = length(hit.position - current_ray.o);
+			vec3 transmittance = exp(-sigma_t * distance);
+			path_throughput *= transmittance;		//Calculate absorption/scattering in the medium for refraction
+		}
 
-	Diffuse diffuse(hit.material->m_color);
-	BTDF& mat = diffuse;
-	///////////////////////////////////////////////////////////////////
-	// Calculate Direct Illumination from light.
-	///////////////////////////////////////////////////////////////////
-	Ray nray;				//Added for shadows
-	nray.o = hit.position + hit.geometry_normal * EPSILON; //ray doesn't start exactly on the surface to avoid self-intersection
-	nray.d = normalize(point_light.position - hit.position);
-	if (!occluded(nray)) {									//if occluded, the point is in shadow and we don't add any contribution from the light source
+		///////////////////////////////////////////////////////////////////
+		// Create a Material tree for evaluating brdfs and calculating
+		// sample directions.
+		///////////////////////////////////////////////////////////////////
+
+		/*Diffuse diffuse(hit.material->m_color);								//Full (without refractions)
+		MicrofacetBRDF microfacet(hit.material->m_shininess);
+		DielectricBSDF dielectric(&microfacet, &diffuse, hit.material->m_fresnel);
+		MetalBSDF metal(&microfacet, hit.material->m_color, hit.material->m_fresnel);
+		BSDFLinearBlend metal_blend(hit.material->m_metalness, &metal, &dielectric);
+		BSDF& mat = metal_blend; */
+
+		//GlassBTDF glass(hit.material->m_ior);		//Glass only
+		//BTDF& mat = glass;
+
+		Diffuse diffuse(hit.material->m_color);								//Full (with refractions)
+		GlassBTDF glass(hit.material->m_ior);
+		BTDFLinearBlend glass_blend(hit.material->m_transparency, &glass, &diffuse);
+		MicrofacetBRDF microfacet(hit.material->m_shininess);
+		DielectricBSDF dielectric(&microfacet, &glass_blend, hit.material->m_fresnel);
+		MetalBSDF metal(&microfacet, hit.material->m_color, hit.material->m_fresnel);
+		BSDFLinearBlend metal_blend(hit.material->m_metalness, &metal, &dielectric);
+		BSDF& mat = metal_blend;
+
+
+		//Diffuse diffuse(hit.material->m_color);		//Diffuse only
+		//BTDF& mat = diffuse;
+
+		/*Diffuse diffuse(hit.material->m_color);			//Dielectric only
+		MicrofacetBRDF microfacet(hit.material->m_shininess);
+		DielectricBSDF dielectric(&microfacet, &diffuse, hit.material->m_fresnel);
+		BSDF& mat = dielectric;*/
+
+		///////////////////////////////////////////////////////////////////
+		// Calculate Direct Illumination from light.
+		///////////////////////////////////////////////////////////////////
+		Ray nray;				//Added for shadows
+		nray.o = hit.position + hit.geometry_normal * EPSILON; //ray doesn't start exactly on the surface to avoid self-intersection
+		nray.d = normalize(point_light.position - hit.position);
+											//if occluded, the point is in shadow and we don't add any contribution from the light source
 		const float distance_to_light = length(point_light.position - hit.position);
 		const float falloff_factor = 1.0f / (distance_to_light * distance_to_light);
 		vec3 Li = point_light.intensity_multiplier * point_light.color * falloff_factor;
 		vec3 wi = normalize(point_light.position - hit.position);
-		L = mat.f(wi, hit.wo, hit.shading_normal) * Li * std::max(0.0f, dot(wi, hit.shading_normal));
+		vec3 direct_illumination = mat.f(wi, hit.wo, hit.shading_normal) * Li * std::max(0.0f, dot(wi, hit.shading_normal));
+		
+		if (!occluded(nray)) {
+			L += path_throughput * direct_illumination;
+		}
+		
+		L += path_throughput * hit.material->m_emission;
+		WiSample sample = mat.sample_wi(hit.wo, hit.shading_normal);
+
+		if (sample.pdf < EPSILON || path_throughput == vec3(0))
+			return L;
+
+		float cosineterm = abs(dot(sample.wi, hit.shading_normal));
+		path_throughput = path_throughput * (sample.f * cosineterm) / sample.pdf;
+
+
+		Ray new_ray;
+		if(dot(sample.wi, hit.geometry_normal) < 0.0f)
+		{
+			new_ray.o = hit.position - hit.geometry_normal * EPSILON;
+			inside = true;		//If the ray goes inside the object, we set inside to true
+			sigma_t = (vec3(1.0f) - hit.material->m_color)*0.1f;		//Set sigma to the opposite of color of the material for refraction and scale it so that it's not too strong (the 0.1f is just a value I found to give good results, it can be changed to get more or less absorption in the medium for refraction)
+			//it could also be set depending on the material but for simplicity (not change all materials to have a sigma value) I just set it depending on the color of the material, so that darker materials have more absorption and lighter materials have less absorption for refraction
+		}
+		else
+		{
+			new_ray.o = hit.position + hit.geometry_normal * EPSILON;
+			inside = false;		//If the ray goes outside the object, we set inside to false
+			sigma_t = vec3(0.0f);		//No absorption outside the object for refraction
+		}
+		new_ray.d = sample.wi;
+		current_ray = new_ray;
+
+		if (!intersect(current_ray))
+			return L + path_throughput * Lenvironment(current_ray.d);
+
+
 	}
 	// Return the final outgoing radiance for the primary ray
 	return L;
--- pathtracer/material.cpp
+++ pathtracer/material.cpp
@@ -38,61 +38,166 @@ WiSample Diffuse::sample_wi(const vec3& wo, const vec3& n) const
 
 vec3 MicrofacetBRDF::f(const vec3& wi, const vec3& wo, const vec3& n) const
 {
-	return vec3(0.0f);
+	if (!sameHemisphere(wi, wo, n))		//reject directions below the surface
+		return vec3(0.0f);
+	vec3 wh = normalize(wi + wo);
+
+	vec3 N;
+	if (dot(wo, n) > 0.0f)
+	{
+		N = n;
+	}
+	else
+	{
+		N = -n;
+	}
+	
+	float n_dot_wi = max(0.0001f, dot(N, wi));
+	float n_dot_wo = max(0.0001f, dot(N, wo));
+	float wo_dot_wh = max(0.0001f, dot(wo, wh));
+	float n_dot_wh = max(0.0001f, dot(N, wh));
+
+
+	float D = (shininess + 2) / (2 * M_PI) * pow(n_dot_wh, shininess);
+	float G = min(1.0f, min(2 * n_dot_wh * n_dot_wo / wo_dot_wh, 2 * n_dot_wh * n_dot_wi / wo_dot_wh));
+	float denominator = 4 * n_dot_wi * n_dot_wo;
+	vec3 BRDF = vec3(D * G / denominator);
+
+	return BRDF;
 }
 
 WiSample MicrofacetBRDF::sample_wi(const vec3& wo, const vec3& n) const
 {
-	WiSample r = sampleHemisphereCosine(wo, n);
-	r.f = f(r.wi, wo, n);
+	//Generate half angle wh								//ADDED to sample a half angle wh according to the distribution of the microfacet normals
+	vec3 N;
+	if (dot(wo, n) > 0.0f)
+	{
+		N = n;
+	}
+	else
+	{
+		N = -n;
+	}
+	WiSample r;
+	vec3 tangent = normalize(perpendicular(N));
+	vec3 bitangent = normalize(cross(tangent, N));
+	float phi = 2.0f * M_PI * randf();
+	float cos_theta = pow(randf(), 1.0f / (shininess + 1));
+	float sin_theta = sqrt(max(0.0f, 1.0f - cos_theta * cos_theta));
+	vec3 wh = normalize(sin_theta * cos(phi) * tangent +
+						sin_theta * sin(phi) * bitangent +
+						cos_theta * N);
+
+	vec3 wi = reflect(-wo, wh);
+	
+	if (dot(wi, N) <= 0.0f || dot(wo, wh) <= 0.0f) {		//reject bad samples
+		r.wi = vec3(0.0);
+		r.pdf = 0.0;
+		r.f = vec3(0.0);
+		return r;
+	}
+
+	float p_wh = (this->shininess + 1.0f) * pow(max(0.0f, dot(N, wh)), this->shininess) / (2.0f * M_PI);	
+	float wh_dot_wo = max(0.0001f, dot(wh, wo));
+	float p_wi = p_wh / (4.0f * wh_dot_wo);
+	r.wi = wi;
+
+	r.f = f(r.wi, wo, N);
+	r.pdf = p_wi;
+
+
 
 	return r;
 }
 
 
-float BSDF::fresnel(const vec3& wi, const vec3& wo) const
+float BSDF::fresnel(const vec3& wi, const vec3& wo) const			//ADDED to calculate the fresnel term for the dielectric bsdf
 {
-	return 0.0f;
+	float R0 = this->R0;
+	vec3 wh = normalize(wi + wo);
+	float wh_dot_wo = dot(wh, wo);			//needed?
+	float F = R0 + (1.0f - R0) * pow(1.0f - wh_dot_wo, 5);
+	return clamp(F, 0.0f, 1.0f);
 }
 
 
-vec3 DielectricBSDF::f(const vec3& wi, const vec3& wo, const vec3& n) const
+vec3 DielectricBSDF::f(const vec3& wi, const vec3& wo, const vec3& n) const		//ADDED to calculate the value of the dielectric bsdf for specific directions
 {
-	return vec3(0);
+	vec3 BSDF;
+	float fresnel = this->fresnel(wi, wo);
+	BSDF = fresnel * reflective_material->f(wi, wo, n) + (1.0f - fresnel) * transmissive_material->f(wi, wo, n);
+
+	return BSDF;
 }
 
 WiSample DielectricBSDF::sample_wi(const vec3& wo, const vec3& n) const
 {
+	vec3 N;
+	if (dot(wo, n) > 0.0f)
+	{
+		N = n;
+	}
+	else
+	{
+		N = -n;
+	}
 	WiSample r;
+	float F = this->fresnel(r.wi, wo);
 
-	r = sampleHemisphereCosine(wo, n);
-	r.f = f(r.wi, wo, n);
+	if(randf()<0.5f)
+	{
+		r = reflective_material->sample_wi(wo, N);
+		r.pdf *= 0.5f;
+		r.f *= F;
+	}
+	else
+	{
+		r = transmissive_material->sample_wi(wo, n);	//original normal so that transmissive_material can determine what side of the object is the outside
+		r.pdf *= 0.5f;
+		r.f *= (1.0f - F);
+	}
 
 	return r;
 }
 
-vec3 MetalBSDF::f(const vec3& wi, const vec3& wo, const vec3& n) const
+vec3 MetalBSDF::f(const vec3& wi, const vec3& wo, const vec3& n) const		//ADDED to calculate the value of the metal bsdf for specific directions
 {
-	return vec3(0);
+	vec3 BSDF;
+	float fresnel = this->fresnel(wi, wo);
+	BSDF = fresnel * reflective_material->f(wi, wo, n) * color;		//multiplied by color because metal absorbs some light
+
+	return BSDF;
 }
 
 WiSample MetalBSDF::sample_wi(const vec3& wo, const vec3& n) const
 {
 	WiSample r;
-	r = sampleHemisphereCosine(wo, n);
-	r.f = f(r.wi, wo, n);
+	r = reflective_material->sample_wi(wo, n);
+	float fresnel = this->fresnel(r.wi, wo);
+	r.f = r.f*fresnel*color;
 	return r;
 }
 
 
-vec3 BSDFLinearBlend::f(const vec3& wi, const vec3& wo, const vec3& n) const
+vec3 BSDFLinearBlend::f(const vec3& wi, const vec3& wo, const vec3& n) const		//ADDED to calculate the value of the linear blend bsdf for specific directions
 {
-	return vec3(0.0);
+	vec3 blend = w * bsdf0->f(wi, wo, n) + (1.0f - w) * bsdf1->f(wi, wo, n);
+	return blend;
 }
 
 WiSample BSDFLinearBlend::sample_wi(const vec3& wo, const vec3& n) const
 {
-	return WiSample{};
+	WiSample r;
+	if (randf() < w)
+	{
+		r = bsdf0->sample_wi(wo, n);
+	}
+	else
+	{
+		r = bsdf1->sample_wi(wo, n);
+	}
+
+	return r;
 }
 
 
@@ -145,7 +250,7 @@ WiSample GlassBTDF::sample_wi(const vec3& wo, const vec3& n) const
 	if(k < 0.0f)
 	{
 		// Total internal reflection
-		r.wi = reflect(-wo, n);
+		r.wi = reflect(-wo, N);
 	}
 	else
 	{
